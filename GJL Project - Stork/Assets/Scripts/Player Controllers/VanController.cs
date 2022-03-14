6017b02fc8b4d97d20e80fc669d09da09571e7f5
PriceyPixel <jamesprice47@gmail.com>
2022-03-14 18:39:21 +0000

    Initial Commit

---
+++ GJL Project - Stork/Assets/Scripts/Player Controllers/VanController.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VanController : MonoBehaviour
+{
+    private void FixedUpdate()
+    {
+        GetInput
+    }
+}
